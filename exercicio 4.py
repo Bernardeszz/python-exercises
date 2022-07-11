3a8ffb7c18f8af3115a5e916c7f4a1ecbb648736
Andryws Oliveira <108766312+Bernardeszz@users.noreply.github.com>
2022-07-11 04:05:42 -0300

    Add files via upload

---
+++ exercicio 4.py
@@ -0,0 +1,9 @@
+#Leia um número real e imprima o quadrado desse número
+
+print ('digite um número real')
+
+real = float(input())
+
+quad = real **2
+
+print(f'o quadrado desse número é {quad}')
---
+++ exercicio 5.py
@@ -0,0 +1,6 @@
+#Leia um número real e imprima a 5ª parte deste número
+
+print ('digite um número real')
+num = float (input())
+
+print (f'o número real dividido por 5 é {num/5}')
